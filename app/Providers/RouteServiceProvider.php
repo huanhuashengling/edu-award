294db603100b0e5b1ee95be9b7688e3cc9f2cc93
ywj <shengling_2005@163.com>
2020-06-02 09:26:18 +0800

    update redirect after login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/list';
+    public const HOME = '/award/list';
 
     /**
      * Define your route model bindings, pattern filters, etc.
